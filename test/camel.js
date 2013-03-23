8987c4bd9d2ca798551cf01443ddcfe6a3ebf5f9
James Halliday <mail@substack.net>
2013-03-22 18:52:46 -0700

    a passing test

---
+++ test/camel.js
@@ -0,0 +1,22 @@
+var test = require('tape');
+var camelize = require('../');
+
+var obj = {
+    fee_fie_foe: 'fum',
+    beep_boop: [
+        { 'abc.xyz': 'mno' },
+        { 'foo-bar': 'baz' }
+    ]
+};
+
+test('camelize a nested object', function (t) {
+    t.plan(1);
+    var res = camelize(obj);
+    t.deepEqual(res, {
+        "feeFieFoe": "fum",
+        "beepBoop": [
+            { "abcXyz": "mno" },
+            { "fooBar": "baz" }
+        ]
+    });
+});
